ef701d35bd34621e94d4719043bd13f375232af8
Daniil <d.tsibenko.exceedteam@gmail.com>
2021-03-29 18:13:36 +0300

    drag and drop

--- src/components/MovieBlock/MovieBlock.jsx
+++ src/components/MovieBlock/MovieBlock.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import MovieCard from "../MovieCard/MovieCard";
 import './MovieBlock.css';
 import {Button, Grid, Paper, TextField} from "@material-ui/core";
@@ -6,6 +6,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import AddMoviePopup from "../AddMoviePopup/AddMoviePopup";
 import db from '../../db.json';
 import EditMoviePopup from "../EditMoviePopup/EditMoviePopup";
+import axios from "axios";
 
 
 
@@ -14,12 +15,12 @@ const MovieBlock = () => {
     const [togglePopup, setTogglePopup] = useState(false)
     const [title, setTitle] = useState('')
 
-
+    console.log(db.posts)
+    const [cardList, setCardList] = useState(db.posts)
     const showPopup = (id) => {
         setTogglePopup(!togglePopup)
         setMovieId(id)
     }
-
 
     const categories = ['Рекомендуем вам посмотреть', 'Фильмы, основанные на реальных событиях',
         'Мультфильмы для всех возрастов',
@@ -39,7 +40,49 @@ const MovieBlock = () => {
         },
     }));
 
+    const [currentCard, setCurrentCard] = useState(null)
 
+    const dragStartHandler = (e, card) => {
+        console.log(e, 'drag ' ,  card)
+        setCurrentCard(card)
+    }
+
+
+    const dragOverHandler = (e) => {
+        e.preventDefault()
+        e.target.style.background = 'lightgray'
+    }
+
+    const dropHandler = (e, card) => {
+        e.preventDefault()
+        console.log('=========card' , card)
+        setCardList(cardList.map(c => {
+            if (c.id === card.id) {
+                return {...c , order: currentCard.order}
+            }
+            if (c.id === currentCard.id) {
+                return {...c , order: card.order}
+            }
+            return c;
+        }))
+        e.target.style.background = 'white'
+
+      //  setCardList(cardList.map(c => {
+      //      if (c.id === card.id) {
+     //           console.log(c.id , card.id)
+      //      }
+      //      return c;
+     //   }))
+
+    }
+
+    const sortCards = (a, b) => {
+        if (a.order > b.order) {
+            return 1
+        } else {
+            return -1
+        }
+    }
 
     const classes = useStyles();
     return (
@@ -61,24 +104,27 @@ const MovieBlock = () => {
                             <p className={classes.title}>{item}</p>
                         </div>
                         <Grid container spacing={3}>
-                            {db.posts.map(card =>
-                                <Grid item>
-                                    <div
-                                    draggable={true}>
-                                        <MovieCard
-                                            image={card.img}
-                                            title={card.title}
-                                            id={card.id}
-                                            togglePopup={togglePopup}
-                                            setTogglePopup={setTogglePopup}
-                                            showPopup={showPopup}
-                                        />
-                                    </div>
-                                </Grid>
-                            )}
+                            {cardList.sort(sortCards).map(card => {
+                                // console.log(card, 'vdffd');
 
-
-
+                                // <Grid item>
+                                return <div
+                                    draggable={true}
+                                    onDragStart={(e) => dragStartHandler(e, card)}
+                                    onDragOver={(e) => dragOverHandler(e)}
+                                    onDrop={(e) => dropHandler(e, card)}
+                                >
+                                    <MovieCard
+                                        image={card.img}
+                                        title={card.title}
+                                        id={card.id}
+                                        togglePopup={togglePopup}
+                                        setTogglePopup={setTogglePopup}
+                                        showPopup={showPopup}
+                                    />
+                                </div>
+                                //  </Grid>
+                            })}
                         </Grid>
 
                     </div>
